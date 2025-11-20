41330b9ac393dc462a3e91b348b5f54c54ce0709
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-11-19 18:46:37 -0600

    Obtener la extensión de un archivo.

---
+++ src/EjemploStringExtensionArchivo.java
@@ -0,0 +1,9 @@
+public class EjemploStringExtensionArchivo {
+    public static void main(String[] args) {
+        String archivo = "alguna.imagen.pdf";
+        int i = archivo.lastIndexOf(".");
+
+        System.out.println("archivo.length() = " + archivo.length());
+        System.out.println("archivo.substring(i+1) = " + archivo.substring(i+1));
+    }
+}
