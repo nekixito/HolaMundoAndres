6d78dc72007504ce138e2e3660542b8d756ae209
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-11-22 09:09:34 -0600

    Operadores Aritméticos.

---
+++ src/OperadorAritmetico.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+
+public class OperadorAritmetico {
+    public static void main(String[] args) {
+        int i = 5, j = 4, suma = i + j;
+        System.out.println("suma = " + suma);
+
+        System.out.println("i + j = " + (i + j));
+        
+        int resta = i - j;
+        System.out.println("resta = " + resta);
+        System.out.println("i - j = " + (i - j));
+        
+        int multiplicacion = i * j;
+        System.out.println("multiplicacion = " + multiplicacion);
+
+        int division = i / j;
+        System.out.println("division = " + division);
+
+        float division2 = (float)i / j;
+        System.out.println("division2 = " + division2);
+        
+        int resto = i % j;
+        System.out.println("resto = " + resto);
+
+        resto = 8 % 5;
+        System.out.println("resto2 = " + resto);
+
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Ingrese un numero: "));
+
+        if(numero % 2 == 0){
+            System.out.println("numero par = " + numero);
+        }else{
+            System.out.println("numero impar = " + numero);
+        }
+    }
+}
