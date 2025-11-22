d4b3f9dbda77ad8caf4860325fff002bb6917a81
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-11-22 09:11:17 -0600

    Cambio de nombre de archivo en Operadores Aritméticos.

--- src/OperadoresAritmeticos.java
+++ src/OperadoresAritmeticos.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 
-public class OperadorAritmetico {
+public class OperadoresAritmeticos {
     public static void main(String[] args) {
         int i = 5, j = 4, suma = i + j;
         System.out.println("suma = " + suma);
