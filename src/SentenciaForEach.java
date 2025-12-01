4339cd4e99e1178c6e83ddf02f237a93f5e9d5e4
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-01 11:16:08 -0600

    Sentencia foreach.

---
+++ src/SentenciaForEach.java
@@ -0,0 +1,15 @@
+public class SentenciaForEach {
+    public static void main(String[] args) {
+        int[] numeros = {1, 3, 5, 7, 9, 11, 13, 14};
+
+        for (int num : numeros){
+            System.out.println("num = " + num);
+        }
+
+        String[] nombres = {"Miguel", "Pepe", "Maria" , "Paco", "Lalo", "Bea", "Pato", "Pepa"};
+
+        for(String nombre: nombres){
+            System.out.println("nombre = " + nombre);
+        }
+    }
+}
