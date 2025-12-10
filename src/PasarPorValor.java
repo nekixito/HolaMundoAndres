66bb3b99047b903f628c42ef3607b58cfa1e3de9
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-10 07:56:57 -0600

    Pasar argumento por valor.

---
+++ src/PasarPorValor.java
@@ -0,0 +1,19 @@
+public class PasarPorValor {
+    public static void main(String[] args) {
+
+        Integer i = 10;
+
+        System.out.println("iniciamos el método main con i = " + i);
+
+        test(i);
+
+        System.out.println("Finaliza el metodo main con i = " + i);
+
+    }
+
+    public static void test(Integer i){
+        System.out.println("Iniciamos el metodo test con i = " + i);
+        i = 35;
+        System.out.println("Finaliza el metodo test con i = " + i);
+    }
+}
