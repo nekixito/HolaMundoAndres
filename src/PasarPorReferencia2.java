8d751f26319956f7e7bd240f82d0cae797dc6808
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-10 08:19:45 -0600

    Pasar argumento por referencia, objeto persona.

---
+++ src/PasarPorReferencia2.java
@@ -0,0 +1,40 @@
+class Persona{
+    private String nombre;
+
+    public void modificarNombre(String nuevoNombre){
+        this.nombre = nuevoNombre;
+    }
+
+    public String leerNombre(){
+        return this.nombre;
+    }
+}
+
+public class PasarPorReferencia2 {
+    public static void main(String[] args) {
+
+        Persona persona = new Persona();
+        persona.modificarNombre("Miguel");
+
+        System.out.println("iniciamos el método main");
+
+        System.out.println("persona.nombre = " + persona.leerNombre());
+
+        System.out.println("Antes de llamar al método test");
+        test(persona);
+        System.out.println("Despues de llamar al método test");
+
+        System.out.println("persona.nombre = " + persona.leerNombre());
+        
+        System.out.println("Finaliza el método main con los datos de la persona modificados!");
+    }
+
+    public static void test(Persona persona){
+        System.out.println("Iniciamos el metodo test");
+
+        persona.modificarNombre("Pepe");
+
+
+        System.out.println("Finaliza el metodo test");
+    }
+}
