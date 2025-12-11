68b0c04dfcd6945199f2d0890c51f7ea079aa4ea
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-11 17:50:43 -0600

    Tiempo en milisegundos con método getTime().

--- src/EjemploJavaUtilDate.java
+++ src/EjemploJavaUtilDate.java
@@ -11,5 +11,20 @@ public class EjemploJavaUtilDate {
 
         String fechaStr = sdf.format(fecha);
         System.out.println("fechaStr = " + fechaStr);
+
+        long j = 0;
+        for (int i = 0; i < 100000000; i++) {
+            j +=i;
+        }
+
+        System.out.println("j = " + j);
+
+        Date fecha2 = new Date();
+
+        long tiempoFinal = fecha2.getTime() - fecha.getTime();
+
+        System.out.println("tiempoFinal = " + tiempoFinal);
+
+
     }
 }
