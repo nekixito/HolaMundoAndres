8f975023a8292d3decea71169703ad90fb420185
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-17 10:15:47 -0600

    Obtener las variables de ambiente del Sistema Operativo.

---
+++ src/EjemploVariablesDeEntorno.java
@@ -0,0 +1,38 @@
+import java.util.Map;
+
+public class EjemploVariablesDeEntorno {
+    public static void main(String[] args) {
+        Map<String,String> varEnv = System.getenv();
+        System.out.println("Variables de ambiente del sistema = " + varEnv);
+
+        System.out.println("-------Listando variables de entorno del sistema-----------");
+
+        for (String key: varEnv.keySet()){
+            System.out.println(key + " => " + varEnv.get(key));
+        }
+
+        String username = System.getenv("USERNAME");
+        System.out.println("username = " + username);
+
+        String javaHome = System.getenv("JAVA_HOME");
+        System.out.println("javaHome = " + javaHome);
+        
+        String tempDir = System.getenv("TEMP");
+        System.out.println("tempDir = " + tempDir);
+
+        String path = System.getenv("Path");
+        System.out.println("path = " + path);
+
+        /*
+        String[] elementosPath = path.split(";");
+        for (String elemento: elementosPath){
+            System.out.println(elemento);
+        }
+        */
+
+        String path2 = varEnv.get("Path");
+        System.out.println("path2 = " + path);
+
+
+    }
+}
