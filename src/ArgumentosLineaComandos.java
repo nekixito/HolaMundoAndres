7de03a38542a9b898b8cd2a3438740aba12b40c9
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2025-12-19 16:00:39 -0600

    Línea de comandos, compilar y ejecutar con argumentos.

---
+++ src/ArgumentosLineaComandos.java
@@ -0,0 +1,12 @@
+public class ArgumentosLineaComandos {
+    public static void main(String[] args) {
+
+        if(args.length == 0){
+            System.out.println("Debe ingresar argumentos!!");
+            System.exit(-1);
+        }
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Argumentos número " + i + ": " + args[i]);
+        }
+    }
+}
